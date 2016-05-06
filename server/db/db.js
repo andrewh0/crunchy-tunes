ef37ec718863680e4d161108e2a36daa6ea4cac7
Andrew H <aho338@gmail.com>
2016-05-06 14:02:45 -0700

    Modify db settings to use ClearDB

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,14 @@
 var Sequelize = require('sequelize');
 var username = process.env.SQL_USERNAME;
 var password = process.env.SQL_PASSWORD;
-var dbURL = process.env.CLEARDB_DATABASE_URL;
+var dbName = process.env.CLEARDB_DATABASE_NAME;
+var dbHost = process.env.CLEARDB_DATABASE_HOST;
 
-var db = new Sequelize(dbURL, username, password, {
-  define: {timestamps: false}
+var db = new Sequelize(dbName, username, password, {
+  host: dbHost,
+  define: {
+    timestamps: false
+  }
 });
 
 module.exports = db;
